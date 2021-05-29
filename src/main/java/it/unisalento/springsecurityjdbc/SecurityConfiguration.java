0c252bab0c317a6c35f3a12bd112dc906f4f0a60
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2021-05-29 12:14:19 +0200

    Creato DB schema H2 con configurazioni di default e aggiunti 2 utenti Questo schema dato che viene passato all'interno del metodo di authentication verrà utilizzato per autenticare gli utenti

--- src/main/java/it/unisalento/springsecurityjdbc/SecurityConfiguration.java
+++ src/main/java/it/unisalento/springsecurityjdbc/SecurityConfiguration.java
@@ -1,16 +1,35 @@
 package it.unisalento.springsecurityjdbc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication();
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .withDefaultSchema()
+                .withUser(
+                        User.withUsername("user")
+                        .password("pass")
+                        .roles("USER")
+                )
+                .withUser(
+                        User.withUsername("admin")
+                                .password("pass")
+                                .roles("ADMIN")
+                );
     }
 
     @Override
